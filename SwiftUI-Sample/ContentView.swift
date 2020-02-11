26ab4ef84fcffad1b4255e9e0de1ec0846bde912
fkn1017 <kosuke.fukano@gmail.com>
2020-02-11 17:18:38 +0900

    Initial Commit

---
+++ SwiftUI-Sample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-Sample
+//
+//  Created by popy1017 on 2020/02/11.
+//  Copyright © 2020 popy1017. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
